84e189eed7c31b477b889652d95a0eb216cf765a
Manoj Paudel <manoj.765421@bumc.tu.edu.np>
2023-12-13 21:14:22 +0545

    worked on meta class

--- nbt2/myapp/models.py
+++ nbt2/myapp/models.py
@@ -5,6 +5,10 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=1000)
     
+    class Meta:
+        verbose_name = 'Author Name'
+        ordering = ['-name']
+    
     def __str__(self):
         return self.name
     
@@ -13,4 +17,7 @@ class Book(models.Model):
     title = models.CharField(max_length=250)
     author = models.ForeignKey("Author", verbose_name="author", on_delete=models.CASCADE)
     published_date = models.DateField()
+    
+    class Meta:
+        verbose_name = 'Pustak'
 
